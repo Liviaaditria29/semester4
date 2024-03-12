96e55d2fae20e1a4548e2d42b02ab5499e9af79d
Livia Aditria <69287149+Liviaaditria29@users.noreply.github.com>
2024-03-12 15:38:29 +0700

    Add files via upload

---
+++ PBO Praktikum Sesi 3/Percobaan6/src/percobaan6/A.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package percobaan6;
+
+/**
+ *
+ * @author Lenovo
+ */
+class A {
+  String var_a = "Variable A";
+  String var_b = "Variable B";
+  String var_c = "Variable C";
+  String var_d = "Variable D";
+
+  A(){
+    System.out.println("Konstruktor A dijalankan");
+  }
+}
