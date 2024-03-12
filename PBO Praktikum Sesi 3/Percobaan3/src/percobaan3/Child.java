5ce27fbc90c649b228d3faa1149d17d239cdb339
Livia Aditria <69287149+Liviaaditria29@users.noreply.github.com>
2024-03-12 15:36:56 +0700

    Add files via upload

---
+++ PBO Praktikum Sesi 3/Percobaan3/src/percobaan3/Child.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package percobaan3;
+
+/**
+ *
+ * @author Lenovo
+ */
+class Parent {
+    // kosong
+}
+
+public class Child extends Parent {
+    int x;
+    public Child(){
+        x = 5;
+    }
+}
